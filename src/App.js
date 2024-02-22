0ff2969a366b2cbfc53d208e85509c0b8d21e4d4
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-02-22 18:56:42 +0530

    Version-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import uuid from "react-uuid";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  // Global state for notes
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+  const [activeNote, setActiveNote] = useState(false);
+
+  // set in local storage
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  //function to add notes
+  const onAddNote = () => {
+    setNotes([
+      ...notes,
+      {
+        id: uuid(),
+        title: "Untitled Note",
+        body: "",
+        lastModified: Date.now(),
+      },
+    ]);
+  };
+
+  //function to delete notes
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete));
+  };
+
+  //function to fetch active note
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  //function to update notes
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+    setNotes(updatedNotesArray);
+  };
+
+  //function to edit notes
+  const onEditNote = (id) => {
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === id) {
+        return {
+          ...note,
+          lastModified: Date.now(),
+        };
+      } else {
+        return note;
+      }
+    });
+    setNotes(updatedNotesArray);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        onUpdateNote={onUpdateNote}
+        onEditNote={onEditNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
